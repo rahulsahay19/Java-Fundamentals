d019f70146657e0e6c6a86f923da83d4321e7459
rahulsahay19 <rahulsahay19@hotmail.com>
2021-07-05 00:26:19 +0530

    2nd commit

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.name="Tim";
+        player.health = 20;
+        player.weapon="Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.print("Remaining health = " + player.healthRemaining());
+
+        damage = 11;
+        player.loseHealth(damage);
+        System.out.print("Remaining health = " + player.healthRemaining());
+    }
+}
